7f90a1d7f6c5702b55ed26449ebf83145aa8a4af
nullgemm <nullgemm@mailbox.org>
2021-08-29 19:03:20 +0200

    finish the slider widget

--- src/default_widgets/slider/razorbeard_widget_slider.c
+++ src/default_widgets/slider/razorbeard_widget_slider.c
@@ -11,7 +11,546 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <stdio.h>
+// fsm update callbacks
+
+static bool update_idling(struct rzb* rzb, void* data, int event_code, int event_state)
+{
+	bool handled = false;
+	struct rzb_widget* widget = data;
+	struct rzb_widget_slider* slider = widget->data_widget;
+	struct rzb_default_widgets_context* context = slider->context;
+
+	switch (event_code)
+	{
+		case RZB_MOUSE_MOTION:
+		{
+			int x;
+			int y; 
+			int width;
+			int height;
+			int pos_x_circle;
+			int pos_y_circle;
+			int radius_circle;
+			int offset_circle;
+
+			if (slider->vertical == true)
+			{
+				pos_x_circle =
+					widget->x
+						+ (widget->width / 2);
+				pos_y_circle =
+					widget->y
+						+ widget->height
+						- context->sizes_current->radius_edge_border
+						- (widget->height - (2 * context->sizes_current->radius_edge_border))
+							* slider->progress / 100;
+
+				radius_circle = widget->width / 2;
+				offset_circle = widget->width % 2;
+
+				x = pos_x_circle - radius_circle - offset_circle;
+				y = pos_y_circle - radius_circle - offset_circle;
+				width = (2 * radius_circle) + offset_circle;
+				height = (2 * radius_circle) + offset_circle;
+			}
+			else
+			{
+				pos_x_circle =
+					widget->x
+						+ context->sizes_current->radius_edge_border
+						+ (widget->width - (2 * context->sizes_current->radius_edge_border))
+							* slider->progress / 100;
+				pos_y_circle =
+					widget->y
+						+ (widget->height / 2);
+
+				radius_circle = widget->height / 2;
+				offset_circle = widget->height % 2;
+
+				x = pos_x_circle - radius_circle - offset_circle;
+				y = pos_y_circle - radius_circle - offset_circle;
+				width = (2 * radius_circle) + offset_circle;
+				height = (2 * radius_circle) + offset_circle;
+			}
+
+			bool hit =
+				rzb_helper_event_mouse_in_rect(
+					context,
+					x,
+					y,
+					width,
+					height);
+
+			if (hit == true)
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_HOVERING);
+
+				rzb_helper_transition_callback(slider->button_on_area, rzb, widget);
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_KEY_SPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_DRAGGING);
+
+				slider->progress += 1;
+
+				if (slider->progress > 100)
+				{
+					slider->progress = 100;
+				}
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_KEY_BACKSPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_DRAGGING);
+
+				slider->progress -= 1;
+
+				if (slider->progress < 0)
+				{
+					slider->progress = 0;
+				}
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_KEY_UP:
+		case RZB_KEY_DOWN:
+		case RZB_KEY_LEFT:
+		case RZB_KEY_RIGHT:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_nearest_widget(
+					rzb,
+					widget,
+					event_code);
+
+				handled = true;
+			}
+
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
+
+	return handled;
+}
+
+static bool update_hovering(struct rzb* rzb, void* data, int event_code, int event_state)
+{
+	bool handled = false;
+	struct rzb_widget* widget = data;
+	struct rzb_widget_slider* slider = widget->data_widget;
+	struct rzb_default_widgets_context* context = slider->context;
+
+	switch (event_code)
+	{
+		case RZB_MOUSE_CLICK_LEFT:
+		{
+			if (event_state == RZB_STATE_PRESS)
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_DRAGGING);
+
+				rzb_select_widget(
+					rzb,
+					widget);
+
+				if (slider->vertical == true)
+				{
+					slider->selection_offset =
+						context->events_data.mouse_pos_y
+						- (widget->y
+							+ widget->height
+							- context->sizes_current->radius_edge_border
+							- (widget->height - (2 * context->sizes_current->radius_edge_border))
+								* slider->progress / 100);
+				}
+				else
+				{
+					slider->selection_offset =
+						context->events_data.mouse_pos_x
+						- (widget->x
+							+ context->sizes_current->radius_edge_border
+							+ (widget->width - (2 * context->sizes_current->radius_edge_border))
+								* slider->progress / 100);
+				}
+
+				rzb_helper_transition_callback(slider->button_pressed, rzb, widget);
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_MOUSE_MOTION:
+		{
+			int x;
+			int y; 
+			int width;
+			int height;
+			int pos_x_circle;
+			int pos_y_circle;
+			int radius_circle;
+			int offset_circle;
+
+			if (slider->vertical == true)
+			{
+				pos_x_circle =
+					widget->x
+						+ (widget->width / 2);
+				pos_y_circle =
+					widget->y
+						+ widget->height
+						- context->sizes_current->radius_edge_border
+						- (widget->height - (2 * context->sizes_current->radius_edge_border))
+							* slider->progress / 100;
+
+				radius_circle = widget->width / 2;
+				offset_circle = widget->width % 2;
+
+				x = pos_x_circle - radius_circle - offset_circle;
+				y = pos_y_circle - radius_circle - offset_circle;
+				width = (2 * radius_circle) + offset_circle;
+				height = (2 * radius_circle) + offset_circle;
+			}
+			else
+			{
+				pos_x_circle =
+					widget->x
+						+ context->sizes_current->radius_edge_border
+						+ (widget->width - (2 * context->sizes_current->radius_edge_border))
+							* slider->progress / 100;
+				pos_y_circle =
+					widget->y
+						+ (widget->height / 2);
+
+				radius_circle = widget->height / 2;
+				offset_circle = widget->height % 2;
+
+				x = pos_x_circle - radius_circle - offset_circle;
+				y = pos_y_circle - radius_circle - offset_circle;
+				width = (2 * radius_circle) + offset_circle;
+				height = (2 * radius_circle) + offset_circle;
+			}
+
+			bool hit =
+				rzb_helper_event_mouse_in_rect(
+					context,
+					x,
+					y,
+					width,
+					height);
+
+			if (hit == false)
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_IDLING);
+
+				rzb_helper_transition_callback(slider->button_off_area, rzb, widget);
+			}
+
+			handled = true;
+
+			break;
+		}
+		case RZB_KEY_SPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_DRAGGING);
+
+				slider->progress += 1;
+
+				if (slider->progress > 100)
+				{
+					slider->progress = 100;
+				}
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_KEY_BACKSPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_DRAGGING);
+
+				slider->progress -= 1;
+
+				if (slider->progress < 0)
+				{
+					slider->progress = 0;
+				}
+
+				handled = true;
+			}
+
+			break;
+		}
+		case RZB_KEY_UP:
+		case RZB_KEY_DOWN:
+		case RZB_KEY_LEFT:
+		case RZB_KEY_RIGHT:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_nearest_widget(
+					rzb,
+					widget,
+					event_code);
+
+				handled = true;
+			}
+
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
+
+	return handled;
+}
+
+static bool update_dragging(struct rzb* rzb, void* data, int event_code, int event_state)
+{
+	bool handled = false;
+	struct rzb_widget* widget = data;
+	struct rzb_widget_slider* slider = widget->data_widget;
+	struct rzb_default_widgets_context* context = slider->context;
+
+	switch (event_code)
+	{
+		case RZB_MOUSE_CLICK_LEFT:
+		{
+			if (event_state == RZB_STATE_RELEASE)
+			{
+				int x;
+				int y; 
+				int width;
+				int height;
+				int pos_x_circle;
+				int pos_y_circle;
+				int radius_circle;
+				int offset_circle;
+
+				if (slider->vertical == true)
+				{
+					pos_x_circle =
+						widget->x
+							+ (widget->width / 2);
+					pos_y_circle =
+						widget->y
+							+ widget->height
+							- context->sizes_current->radius_edge_border
+							- (widget->height - (2 * context->sizes_current->radius_edge_border))
+								* slider->progress / 100;
+
+					radius_circle = widget->width / 2;
+					offset_circle = widget->width % 2;
+
+					x = pos_x_circle - radius_circle - offset_circle;
+					y = pos_y_circle - radius_circle - offset_circle;
+					width = (2 * radius_circle) + offset_circle;
+					height = (2 * radius_circle) + offset_circle;
+				}
+				else
+				{
+					pos_x_circle =
+						widget->x
+							+ context->sizes_current->radius_edge_border
+							+ (widget->width - (2 * context->sizes_current->radius_edge_border))
+								* slider->progress / 100;
+					pos_y_circle =
+						widget->y
+							+ (widget->height / 2);
+
+					radius_circle = widget->height / 2;
+					offset_circle = widget->height % 2;
+
+					x = pos_x_circle - radius_circle - offset_circle;
+					y = pos_y_circle - radius_circle - offset_circle;
+					width = (2 * radius_circle) + offset_circle;
+					height = (2 * radius_circle) + offset_circle;
+				}
+
+				bool hit =
+					rzb_helper_event_mouse_in_rect(
+						context,
+						x,
+						y,
+						width,
+						height);
+				
+				if (hit == true)
+				{
+					rzb_fsm_button_set_state(
+						&(slider->fsm_slider),
+						RZB_FSM_BUTTON_STATE_HOVERING);
+				}
+				else
+				{
+					rzb_fsm_button_set_state(
+						&(slider->fsm_slider),
+						RZB_FSM_BUTTON_STATE_IDLING);
+				}
+
+				rzb_helper_transition_callback(slider->button_released, rzb, widget);
+				handled = true;
+
+				break;
+			}
+
+			break;
+		}
+		case RZB_MOUSE_MOTION:
+		{ 
+			int mouse;
+			int bar_min;
+			int bar_size;
+
+			int end_radius =
+				context->sizes_current->radius_edge_border;
+
+			if (slider->vertical == true)
+			{
+				mouse = context->events_data.mouse_pos_y;
+				bar_min = widget->y + widget->height - end_radius;
+				bar_size = widget->height - (2 * end_radius);
+			}
+			else
+			{
+				mouse = context->events_data.mouse_pos_x;
+				bar_min = widget->x + end_radius;
+				bar_size = widget->width - (2 * end_radius);
+			}
+
+			slider->progress =
+				100
+				* (mouse - slider->selection_offset - bar_min)
+				/ bar_size;
+
+			if (slider->progress > 100)
+			{
+				slider->progress = 100;
+			}
+			else if (slider->progress < 0)
+			{
+				slider->progress = 0;
+			}
+
+			rzb_helper_transition_callback(slider->button_dragged, rzb, widget);
+
+			widget->render = true;
+
+			break;
+		}
+		case RZB_KEY_SPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				slider->progress += 1;
+
+				if (slider->progress > 100)
+				{
+					slider->progress = 100;
+				}
+
+				handled = true;
+				break;
+			}
+
+			if ((event_state == RZB_STATE_RELEASE)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_IDLING);
+
+				handled = true;
+				break;
+			}
+
+			break;
+		}
+		case RZB_KEY_BACKSPACE:
+		{
+			if ((event_state == RZB_STATE_PRESS)
+			&& (rzb->events_grabber == widget))
+			{
+				slider->progress -= 1;
+
+				if (slider->progress < 0)
+				{
+					slider->progress = 0;
+				}
+
+				handled = true;
+				break;
+			}
+
+			if ((event_state == RZB_STATE_RELEASE)
+			&& (rzb->events_grabber == widget))
+			{
+				rzb_fsm_button_set_state(
+					&(slider->fsm_slider),
+					RZB_FSM_BUTTON_STATE_IDLING);
+
+				handled = true;
+				break;
+			}
+
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
+
+	return handled;
+}
 
 // slider
 
@@ -20,7 +559,12 @@ struct rzb_widget*
 		struct rzb* rzb,
 		void (*callback_layout)(struct rzb*, struct rzb_widget*),
 		struct rzb_default_widgets_context* context,
-		void (*slider_moved)(struct rzb*, struct rzb_widget*),
+		void (*button_on_area)(struct rzb*, struct rzb_widget*),
+		void (*button_off_area)(struct rzb*, struct rzb_widget*),
+		void (*button_pressed)(struct rzb*, struct rzb_widget*),
+		void (*button_released)(struct rzb*, struct rzb_widget*),
+		void (*button_dragged)(struct rzb*, struct rzb_widget*),
+		void* slider_data,
 		bool vertical,
 		uint32_t progress)
 {
@@ -54,10 +598,17 @@ struct rzb_widget*
 	struct rzb_widget_slider slider =
 	{
 		.context = context,
-		.slider_moved = slider_moved,
-		.pushed = false,
+
+		.button_on_area = button_on_area,
+		.button_off_area = button_off_area,
+		.button_pressed = button_pressed,
+		.button_released = button_released,
+		.button_dragged = button_dragged,
+		.slider_data = slider_data,
+
 		.vertical = vertical,
 		.progress = progress,
+		.selection_offset = 0,
 	};
 
 	// prepare persistent memory area
@@ -73,6 +624,19 @@ struct rzb_widget*
 	// naive-copy the data and link it to the widget
 	*data = slider;
 	widget->data_widget = data;
+
+	// prepare the fsm
+	bool (*update[RZB_FSM_BUTTON_STATE_COUNT])(struct rzb* rzb, void*, int, int) =
+	{
+		[RZB_FSM_BUTTON_STATE_IDLING] = update_idling,
+		[RZB_FSM_BUTTON_STATE_HOVERING] = update_hovering,
+		[RZB_FSM_BUTTON_STATE_DRAGGING] = update_dragging,
+	};
+
+	rzb_fsm_button_init(
+		&(data->fsm_slider),
+		widget,
+		update);
 
 	return widget;
 }
@@ -317,48 +881,7 @@ bool rzb_event_widget_slider(
 	int event_code,
 	int event_state)
 {
-#if 0
 	struct rzb_widget_slider* data = widget->data_widget;
-	struct rzb_default_widgets_context* context = data->context;
-#endif
 
-	switch (event_code)
-	{
-		case RZB_KEY_UP:
-		case RZB_KEY_DOWN:
-		case RZB_KEY_LEFT:
-		case RZB_KEY_RIGHT:
-		{
-			if ((event_state == RZB_STATE_PRESS)
-				&& (rzb->events_grabber == widget))
-			{
-				rzb_nearest_widget(
-					rzb,
-					widget,
-					event_code);
-
-				return true;
-			}
-
-			return false;
-		}
-		default:
-		{
-			return false;
-		}
-	}
-}
-
-void rzb_event_widget_slider_move_start(
-	struct rzb* rzb,
-	struct rzb_widget* widget)
-{
-	// TODO
-}
-
-void rzb_event_widget_slider_move_end(
-	struct rzb* rzb,
-	struct rzb_widget* widget)
-{
-	// TODO
+	return rzb_fsm_button_update(rzb, &(data->fsm_slider), event_code, event_state);
 }
--- src/default_widgets/slider/razorbeard_widget_slider.h
+++ src/default_widgets/slider/razorbeard_widget_slider.h
@@ -12,7 +12,12 @@ struct rzb_widget*
 		struct rzb* rzb,
 		void (*callback_layout)(struct rzb*, struct rzb_widget*),
 		struct rzb_default_widgets_context* context,
-		void (*slider_moved)(struct rzb*, struct rzb_widget*),
+		void (*button_on_area)(struct rzb*, struct rzb_widget*),
+		void (*button_off_area)(struct rzb*, struct rzb_widget*),
+		void (*button_pressed)(struct rzb*, struct rzb_widget*),
+		void (*button_released)(struct rzb*, struct rzb_widget*),
+		void (*button_dragged)(struct rzb*, struct rzb_widget*),
+		void* handles_data,
 		bool vertical,
 		uint32_t progress);
 
@@ -31,23 +36,22 @@ bool rzb_event_widget_slider(
 	int event_code,
 	int event_state);
 
-void rzb_event_widget_slider_move_start(
-	struct rzb* rzb,
-	struct rzb_widget* widget);
-
-void rzb_event_widget_slider_move_end(
-	struct rzb* rzb,
-	struct rzb_widget* widget);
-
 struct rzb_widget_slider
 {
 	struct rzb_default_widgets_context* context;
 
-	void (*slider_moved)(struct rzb*, struct rzb_widget*);
+	void (*button_on_area)(struct rzb*, struct rzb_widget*);
+	void (*button_off_area)(struct rzb*, struct rzb_widget*);
+	void (*button_pressed)(struct rzb*, struct rzb_widget*);
+	void (*button_released)(struct rzb*, struct rzb_widget*);
+	void (*button_dragged)(struct rzb*, struct rzb_widget*);
+	void* slider_data;
 
-	bool pushed;
+	struct rzb_fsm_button fsm_slider;
+
 	bool vertical;
-	uint32_t progress; // 16.16 fixed-point
+	int progress;
+	int selection_offset;
 };
 
 #endif
